7af0ee6a45ffe13be5e472ffc3bb898d9e1453a9
qwert26765 <3895675652@qq.com>
2026-01-07 13:59:16 +0800

    宋毅鑫界面打开

---
+++ HotFix/UI/HorseFeedWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HotFix
+{
+    internal class HorseFeedWindow:Window
+    {
+        private Button Money;
+        private Button Close;
+
+
+
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null)
+        {
+            GatAllComponent();
+            AddAllBtnListener();
+        }
+        void AddAllBtnListener()
+        {
+          
+            AddButtonClickListener(Money, ShowMoneyPanel);
+           AddButtonClickListener(Close, Closepanel);
+
+        }
+
+        private void Closepanel()
+        {
+            UIManager.instance.CloseWnd(FilesName.HORSEFEEDPANEL);
+        }
+
+        private void ShowMoneyPanel()
+        {
+            UIManager.instance.PopUpWnd(FilesName.HORSEFEEDPANEL, true, false, Money.transform);
+        }
+        private void GatAllComponent()
+        {
+            Money = m_Transform.Find("Btns/Money").GetComponent<Button>();
+           Close = m_Transform.Find("BackImg/Close").GetComponent<Button>();
+        }
+        
+       
+    }
+}
